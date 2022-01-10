4288f7ee363667ce318080c2b6b053ba5973687d
Bigon4ik <bigon444ik@gmail.com>
2022-01-11 00:17:11 +0300

    add acordion,rating and star

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Acordion from './Acordion';
+import Rating from './Rating';
+import Star from './Star';
 
 function App() {
   return (
     <div >
-     hello
+      <Acordion />
+      <Rating />
+      <Star/>
+
     </div>
   );
 }
