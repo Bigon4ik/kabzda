639fc7b777a205434e9ce64e1582a7e958c3665b
Bigon4ik <bigon444ik@gmail.com>
2022-01-11 00:24:17 +0300

    add star

--- src/Rating.tsx
+++ src/Rating.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Star from './Star';
 
 function App() {
   return (
     <div >
-     hello
+     <Star/>
     </div>
   );
 }
